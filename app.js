51b12eaa737c26c0c1dc0f4ef7e58b0623768be5
DarushDev <darushfaqiri@gmail.com>
2017-06-03 14:29:25 +0800

    Add background image to body based on weather condition

--- app.js
+++ app.js
@@ -21,7 +21,18 @@ var icons = {
     "partly-cloudy-night": "http://openweathermap.org/img/w/02n.png"
 };
 var temperature = 0;
-
+var backgrounds = {
+    "clear-day": "https://pixabay.com/get/eb34b60e2fe90021d85a5840981318c3fe76e6dc1fb517499cf8c6/background-21717_1280.jpg",
+    "clear-night": "https://pixabay.com/get/e034b9082ff31c22d2524518a33219c8b66ae3d110b715479cf0c17c/stars-918777_1280.jpg",
+    "rain": "https://pixabay.com/get/eb37b10c2ef0013ed1584d05fb0938c9bd22ffd41db8174593f9c17ea2/rain-2203640_1280.jpg",
+    "snow": "https://pixabay.com/get/eb35b7072afd093ed1584d05fb0938c9bd22ffd41db8174593f9c378a3/wintry-2068298_1280.jpg",
+    "sleet": "https://pixabay.com/get/e83db5072bf0073ed1584d05fb0938c9bd22ffd41db8174593f9c37ca7/snow-1848346_1280.png",
+    "wind": "https://pixabay.com/get/e83cb10920f7043ed1584d05fb0938c9bd22ffd41db8174593f9c071a5/reed-1906835_1280.jpg",
+    "fog": "https://pixabay.com/get/ec31b60c2ef71c22d2524518a33219c8b66ae3d110b715479cf2c97e/forest-547363_1280.jpg",
+    "cloudy": "https://pixabay.com/get/eb34b8082efc043ed1584d05fb0938c9bd22ffd41db8174593f8c879af/weather-2197685_1280.jpg",
+    "partly-cloudy-day": "https://pixabay.com/get/e833b7092efc073ed1584d05fb0938c9bd22ffd41db8174593f9c27da1/sky-1666686_1280.jpg",
+    "partly-cloudy-night": "https://pixabay.com/get/e83db7062ff1023ed1584d05fb0938c9bd22ffd41db8174593f9c270a3/cloudy-1869753_1280.jpg"
+};
 
 function callWeatherApi(url) {
     var success = function (data) {
@@ -29,6 +40,7 @@ function callWeatherApi(url) {
         $("#icon").attr("src", icons[data.currently.icon]);
         $("#temperature").text(temperature);
         $("#timezone").text(data.timezone);
+        $("body").css("background-image", "url("+backgrounds[data.currently.icon])+")";
     };
     var error = function () {
         alert("error");
